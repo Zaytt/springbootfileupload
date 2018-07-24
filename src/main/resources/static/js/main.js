350229d81bfe3edf161cbc020abe8395d08d512f
Zaytt <ivan_chavez_e@hotmail.com>
2018-07-24 13:44:12 -0500

    Fixed Front-End Uploads

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,58 +1,57 @@
 'use strict';
 
-var singleUploadFS = document.querySelector('#singleUploadForm-fs');
+var singleUploadForm = document.querySelector('#singleUploadForm');
 var singleFileUploadInput = document.querySelector('#singleFileUploadInput');
 var singleFileUploadError = document.querySelector('#singleFileUploadError');
 var singleFileUploadSuccess = document.querySelector('#singleFileUploadSuccess');
 
-var multipleUploadFS = document.querySelector('#multipleUploadForm-fs');
+var multipleUploadForm = document.querySelector('#multipleUploadForm');
 var multipleFileUploadInput = document.querySelector('#multipleFileUploadInput');
 var multipleFileUploadError = document.querySelector('#multipleFileUploadError');
 var multipleFileUploadSuccess = document.querySelector('#multipleFileUploadSuccess');
 
-var singleUploadDB = document.querySelector('#singleUploadForm-db');
-var multipleUploadDB = document.querySelector('#multipleUploadForm-db');
-
-function uploadSingleFile(file, type) {
-
+function uploadSingleFile(file, storage) {
     var formData = new FormData();
     formData.append("file", file);
-
+    var storeloc = '';
     var xhr = new XMLHttpRequest();
-    if(type = 'fsys'){
+    if(storage == 'filesys'){
         xhr.open("POST", "/uploadFile");
-    } else if (type = 'db'){
+        storeloc = 'Filesystem';
+    } else if (storage == 'database'){
         xhr.open("POST", "/uploadFileDB");
+        storeloc = 'Database';
     }
-
 
     xhr.onload = function() {
         console.log(xhr.responseText);
         var response = JSON.parse(xhr.responseText);
         if(xhr.status == 200) {
             singleFileUploadError.style.display = "none";
-            singleFileUploadSuccess.innerHTML = "<p>File Uploaded Successfully.</p><p>DownloadUrl : <a href='" + response.fileDownloadUri + "' target='_blank'>" + response.fileDownloadUri + "</a></p>";
+            singleFileUploadSuccess.innerHTML = "<p>File Uploaded Successfully to the " + storeloc + ".</p><p>DownloadUrl : <a href='" + response.fileDownloadUri + "' target='_blank'>" + response.fileDownloadUri + "</a></p>";
             singleFileUploadSuccess.style.display = "block";
         } else {
             singleFileUploadSuccess.style.display = "none";
-            singleFileUploadError.innerHTML = (response && response.message) || "Some Error Occurred";
+            singleFileUploadError.innerHTML = (response && response.message) || "Some Error Occurred while uploading to the " + storeloc;
         }
     }
 
     xhr.send(formData);
 }
 
-function uploadMultipleFiles(files, type) {
+function uploadMultipleFiles(files, storage) {
     var formData = new FormData();
     for(var index = 0; index < files.length; index++) {
         formData.append("files", files[index]);
     }
-
     var xhr = new XMLHttpRequest();
-    if(type = 'fsys'){
-        xhr.open("POST", "/uploadFile");
-    } else if (type = 'db'){
-        xhr.open("POST", "/uploadFileDB");
+    var storeloc = '';
+    if(storage == 'filesys'){
+        xhr.open("POST", "/uploadMultipleFiles");
+        storeloc = 'Filesystem';
+    } else if (storage == 'database'){
+        xhr.open("POST", "/uploadMultipleFilesDB");
+        storeloc = 'Database';
     }
 
     xhr.onload = function() {
@@ -68,72 +67,42 @@ function uploadMultipleFiles(files, type) {
             multipleFileUploadSuccess.style.display = "block";
         } else {
             multipleFileUploadSuccess.style.display = "none";
-            multipleFileUploadError.innerHTML = (response && response.message) || "Some Error Occurred";
+            multipleFileUploadError.innerHTML = (response && response.message) || "Some Error Occurred while uploading to the " + storeloc;
         }
     }
 
     xhr.send(formData);
 }
 
-singleUploadFS.addEventListener('submit', function(event){
+
+singleUploadForm.addEventListener('submit', function(event){
     var files = singleFileUploadInput.files;
     if(files.length === 0) {
         singleFileUploadError.innerHTML = "Please select a file";
         singleFileUploadError.style.display = "block";
     }
-    uploadSingleFile(files[0], 'fsys');
+    var storeloc;
+    if (document.getElementById('rs1').checked)
+        storeloc = document.getElementById('rs1').value;
+    else
+        storeloc = document.getElementById('rs2').value;
+
+    uploadSingleFile(files[0], storeloc);
     event.preventDefault();
 }, true);
 
 
-multipleUploadFS.addEventListener('submit', function(event){
+multipleUploadForm.addEventListener('submit', function(event){
     var files = multipleFileUploadInput.files;
     if(files.length === 0) {
         multipleFileUploadError.innerHTML = "Please select at least one file";
         multipleFileUploadError.style.display = "block";
     }
-    uploadMultipleFiles(files, 'fsys');
+    var storeloc;
+    if (document.getElementById('rm1').checked)
+        storeloc = document.getElementById('rm1').value;
+    else
+        storeloc = document.getElementById('rm2').value;
+    uploadMultipleFiles(files, storeloc);
     event.preventDefault();
 }, true);
-
-singleUploadDB.addEventListener('submit', function(event){
-    var files = singleFileUploadInput.files;
-    if(files.length === 0) {
-        singleFileUploadError.innerHTML = "Please select a file";
-        singleFileUploadError.style.display = "block";
-    }
-    uploadSingleFile(files[0], 'db');
-    event.preventDefault();
-}, true);
-
-
-multipleUploadDB.addEventListener('submit', function(event){
-    var files = multipleFileUploadInput.files;
-    if(files.length === 0) {
-        multipleFileUploadError.innerHTML = "Please select at least one file";
-        multipleFileUploadError.style.display = "block";
-    }
-    uploadMultipleFiles(files, 'db');
-    event.preventDefault();
-}, true);
-
-function openTab(event, tabName){
-    // Declare all variables
-    var i, tabcontent, tablinks;
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
-}
-
